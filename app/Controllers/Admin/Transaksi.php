e21f07a8bce344bb1f44442440bdf32ea5559c45
kritingkenway <adityamakbar123@gmail.com>
2023-06-07 14:16:32 +0700

    sedikit edit

--- app/Controllers/Admin/Transaksi.php
+++ app/Controllers/Admin/Transaksi.php
@@ -9,7 +9,7 @@ class Transaksi extends BaseController
     public function index()
     {
         $data = [
-            'title' => 'Kelola Produk',
+            'title' => 'List Transaksi',
             'transaksi' => $this->db
                 ->table('tbl_transaksi')
                 ->join('tbl_user', 'tbl_user.id_user = tbl_transaksi.id_user', 'left')
